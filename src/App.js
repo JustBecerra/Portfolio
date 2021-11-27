e42d95e608f8f66876b0d63a6253d770823c1767
JustBecerra <justj.becerra@gmail.com>
2021-11-27 16:17:10 -0300

    avance un poco

--- src/App.js
+++ src/App.js
@@ -6,18 +6,56 @@ import Techs from './components/PersonalInfo/Techs'
 import Proyects from './components/Proyects/Proyects'
 import Footer from './components/Footer/Footer'
 import '../src/components/Proyects/proyects.css'
+import { Box, Typography } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
 function App() {
+
+  const theme = createTheme({
+    typography: {
+      palette: {
+        // primary: {
+        //   main: '#00cc44',
+        //   dark: '#000000',
+        // },
+      },
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+  });
+
   return (
     <div>
        <div> 
-         {/* <img className='backGround' src={backG} alt="nothing"/> */}
          <img className='profilePic' src={pic} alt="nothing"/>
-         <p className='welcometext'>Hello there,<br/>
-            I'm Justo Becerra<br/>
-            Full Stack Developer<br/>
-          </p>
-          <h3 className='residencetext'>residing in Argentina</h3>
+           <Typography variant="p" className='welcometext'>
+             <ThemeProvider theme={theme}>
+              <Box fontFamily='BlinkMacSystemFont' color='primary.dark' fontSize='65px'>
+              Hello there,<br/>
+              I'm Justo Becerra<br/>
+              Full Stack Developer<br/>
+              
+              </Box>
+              </ThemeProvider>
+            </Typography>
+            <Typography variant='h5' className='residencetext' >
+            <ThemeProvider theme={theme}>
+              <Box fontFamily='BlinkMacSystemFont' text-underline='1px'>
+               residing in Argentina
+              </Box>
+              </ThemeProvider>
+            </Typography>
        </div>
        <div>
          <Intro/>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,42 @@
 import './footer.css'
 import logoGitHub from '../imgs/logoGitHub.png'
 import logoLinkedIn from '../imgs/logoLinkedIn.png'
+import logoGmail from '../imgs/logoGmail.png'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+
 
 export default function Footer(){
     return(
+       <Container >
         <div className='footer'>
-          <div  className='textito'>
-            <h2>want to reach out? here you go</h2>
+          <div className='espaciologos'>
+            <Grid container
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <img className='logoGitHub' src={logoGitHub} alt='no hay nada'/>         
+                <h4 className='githubtext'>
+                 <a href='https://github.com/JustBecerra'>
+                  GitHub
+                 </a>
+                </h4>
+              <img className='logoLinkedIn' src={logoLinkedIn} alt='no hay nada'/>  
+                  <h4 className='linkedtext'>
+                    <a href='https://www.linkedin.com/in/justo-juan-becerra-14868a1b4/'>
+                    LinkedIn
+                    </a>
+                  </h4>
+                
+                <img className='logoGmail' src={logoGmail} alt='no hay nada'/>
+                <h4 >
+                  justj.becerra@gmail.com
+                </h4>
+            </Grid>
           </div>
-          <div className='cajalogos'>
-            <a href='https://github.com/JustBecerra'>
-                GitHub
-                <img className='logos' src={logoGitHub} alt='no hay nada'/>
-            </a>
-            <a href='https://www.linkedin.com/in/justo-juan-becerra-14868a1b4/'>
-                LinkedIn
-                <img className='logos' src={logoLinkedIn} alt='no hay nada'/>
-            </a>
-          </div>
+          {/* <h1>Espero que te haya gustado!</h1> */}
         </div>
+        </Container>
     )
 }
