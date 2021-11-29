9f41651fc0f9f0edb8df890e54279c473db50025
JustBecerra <justj.becerra@gmail.com>
2021-11-28 21:02:45 -0300

    techs faltan hover

--- src/components/PersonalInfo/Techs.jsx
+++ src/components/PersonalInfo/Techs.jsx
@@ -1,6 +1,18 @@
 import './techs.css'
 import { Box } from '@material-ui/core'
 import { ListItem } from '@material-ui/core'
+import jslogo from '../imgs/javascriptlogo.png'
+import tslogo from '../imgs/typescriptlogo.png'
+import reactlogo from '../imgs/reactlogo.png'
+import reduxlogo from '../imgs/reduxlogo.png'
+import nodelogo from '../imgs/nodelogo.png'
+import mongologo from '../imgs/mongologo.png'
+import socketlogo from '../imgs/socketlogo.png'
+import sequelizelogo from '../imgs/sequelizelogo.png'
+import postgreslogo from '../imgs/postgreslogo.png'
+import expresslogo from '../imgs/expresslogo.png'
+import htmllogo from '../imgs/htmllogo.png'
+import csslogo from '../imgs/csslogo.png'
 
 export default function Techs(){
     return(
@@ -9,24 +21,60 @@ export default function Techs(){
             <Box
             sx={{
                 display: 'grid',
+                width:600,
                 gap:1,
                 gridTemplateColumns: 'repeat(3, 1fr)',
-                justifyContent: 'space-between'
               }}
               className='box'
             >
-                <ListItem className='singletech'>JavaScript</ListItem>
-                <ListItem className='singletech'>TypeScript</ListItem>
-                <ListItem className='singletech'>ReactJS</ListItem>
-                <ListItem className='singletech'>Redux</ListItem>
-                <ListItem className='singletech'>MongoDB</ListItem>
-                <ListItem className='singletech'>NodeJS</ListItem>
-                <ListItem className='singletech'>WebSocket</ListItem>
-                <ListItem className='singletech'>Express</ListItem>
-                <ListItem className='singletech'>PostgresSQL</ListItem>
-                <ListItem className='singletech'>Sequelize</ListItem>
-                <ListItem className='singletech'>HTML5</ListItem>
-                <ListItem className='singletech'>CSS</ListItem>
+                <ListItem className='singletech'>
+                    JavaScript
+                    <img className='jslogo' src={jslogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    TypeScript
+                    <img className='tslogo' src={tslogo} alt='nada'/>
+                </ListItem>
+                <ListItem className='singletech'>
+                    ReactJS
+                    <img className='htmllogo' src={reactlogo} alt='nada'/>
+                </ListItem>
+                <ListItem className='singletech'>
+                    Redux
+                    <img className='reduxlogo' src={reduxlogo} alt='nada'/>
+                </ListItem>
+                <ListItem className='singletech'>
+                    MongoDB
+                    <img className='mongologo' src={mongologo} alt='nada'/>
+                </ListItem>
+                <ListItem className='singletech'>
+                    NodeJS
+                    <img className='htmllogo' src={nodelogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    WebSocket
+                    <img className='socketlogo' src={socketlogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    Express
+                    <img className='expresslogo' src={expresslogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    PostgresSQL
+                    <img className='socketlogo' src={postgreslogo} alt='nada'/>
+                </ListItem>
+                <ListItem className='singletech'>
+                    Sequelize
+                    <img className='jslogo' src={sequelizelogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    HTML5
+                    <img className='htmllogo' src={htmllogo} alt='nada'/>    
+                </ListItem>
+                <ListItem className='singletech'>
+                    CSS
+                    <img className='csslogo' src={csslogo} alt='nada'/>    
+                </ListItem>
             </Box>
         </div>
     )
